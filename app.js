22248c8ac2879225aa37bdac68ae21fe0c57f641
yeissonpigo <48213359+yeissonpigo@users.noreply.github.com>
2021-04-10 08:08:54 +0300

    Uploading the project. So far, there's a responsive navbar, and some plane text

---
+++ app.js
@@ -0,0 +1,22 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav__links');
+    const navLinks = document.querySelectorAll('.nav__links li');
+
+    burger.addEventListener('click', () => {
+
+        //toggle nav
+        nav.classList.toggle('nav__active');
+        
+        //Link's animations
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = '';
+            }else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+    });
+}
+
+navSlide();
