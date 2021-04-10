1da5114a100cf46208fbdf07bee7cd9567449321
yeissonpigo <48213359+yeissonpigo@users.noreply.github.com>
2021-04-10 17:51:28 +0300

    Navbar updated. Pendent: to correct the responsive on big screens. Small mobiles is already responsive

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const navSlide = () => {
         //Link's animations
         navLinks.forEach((link, index) => {
             if(link.style.animation){
-                link.style.animation = '';
+                link.style.animation = ``;
             }else{
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
             }
